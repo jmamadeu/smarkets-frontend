e074feda588cda0e3347f3e9c034c01b5e6e0c65
jmamadeu <jmamadeu2000@gmail.com>
2021-11-13 05:02:34 +0100

    chore: add .env vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://api.smarkets.com/v3'
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 export { api };
